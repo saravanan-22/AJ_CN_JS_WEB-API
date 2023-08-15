89cd557b20710db18067b0b2e852de112fd12d70
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 21:52:05 -0700

    Updated_Workout-Code

--- localStorage.js
+++ localStorage.js
@@ -3,3 +3,22 @@ console.log(localStorage);
 console.log(localStorage.getItem("AuthorName"));
 //   localStorage.removeItem("AuthorName");
 localStorage.clear("AuthorName");
+
+
+
+
+// localStorage.setItem("AuthorName", "Saravanan");: This line of code uses the localStorage object to set a key-value pair in the web browser's local storage. 
+// The key is "AuthorName" and the value is "Saravanan". 
+// This means that you are storing the author's name "Saravanan" in the local storage under the key "AuthorName".
+
+// console.log(localStorage);: This line prints the entire content of the local storage to the console. 
+// It will display all the key-value pairs that have been stored in the local storage, including the "AuthorName" key and its corresponding value.
+
+// console.log(localStorage.getItem("AuthorName"));: This line uses the getItem() method of the localStorage object to retrieve the value associated with the key "AuthorName" and then prints it to the console. 
+// In this case, it will print "Saravanan" because that's the value you previously set.
+
+// localStorage.removeItem("AuthorName");: This line is commented out with // at the beginning, so it won't be executed. 
+// If uncommented, it would remove the key-value pair with the key "AuthorName" from the local storage. After this line executes, if you were to try retrieving the value associated with "AuthorName", it would return null.
+
+// localStorage.clear("AuthorName");: This line is also commented out. The clear() method is used to remove all key-value pairs from the local storage. It doesn't take any arguments. However, the correct syntax to use clear() is without any arguments, like this: localStorage.clear();. 
+// It would clear all data stored in the local storage, not just the one with the key "AuthorName".
